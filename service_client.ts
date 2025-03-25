8a475ab76e00a016a5b6f1a15eb78eba0e9d9174
Yuxuan Xie <ykuyo.dev@gmail.com>
2025-03-25 14:55:18 -0700

    Fixed importing env vars.

--- service_client.ts
+++ service_client.ts
@@ -1,6 +1,4 @@
-import { CLUSTER_ENV_VARS } from "@phading/cluster/env_vars";
+import { ENV_VARS } from "./env_vars";
 import { NodeServiceClient } from "@selfage/node_service_client";
 
-export let SERVICE_CLIENT = NodeServiceClient.create(
-  CLUSTER_ENV_VARS.internalOrigin,
-);
+export let SERVICE_CLIENT = NodeServiceClient.create(ENV_VARS.internalOrigin);
